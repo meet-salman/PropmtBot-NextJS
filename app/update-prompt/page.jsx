295967b00fea181e8965f84c16a96c82228e9201
Salman Ahmed <say2salmanahmed@gmail.com>
2024-03-30 22:34:16 +0500

    update

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -66,13 +66,18 @@ const EditPrompt = () => {
     }
 
     return (
-        <Form
-            type="Edit"
-            post={post}
-            setPost={setPost}
-            submitting={submitting}
-            handleSubmit={updatePrompt}
-        />
+
+        <Suspense fallback={<div>Loading...</div>}>
+            <Form
+                type="Edit"
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={updatePrompt}
+            />
+        </Suspense>
+
+
     )
 }
 
