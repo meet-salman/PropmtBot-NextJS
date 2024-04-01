11c43e11757f78ea00acc0889935c26022e5c790
Salman Ahmed <say2salmanahmed@gmail.com>
2024-04-02 03:58:34 +0500

    Update

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
     }
-  }
-  
-  module.exports = nextConfig
+    return config
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+}
+
+module.exports = nextConfig
